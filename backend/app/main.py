57327ba27d7a81e74562f58b2079ca60e9a01632
nafisa <nafisatasnim8855@gmail.com>
2026-06-08 17:40:16 +0600

    Modify .env variable for deployed frontend link

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://career-pilot-zeta.vercel.app/"],
+    allow_origins=[os.getenv("FRONTEND_URL")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
